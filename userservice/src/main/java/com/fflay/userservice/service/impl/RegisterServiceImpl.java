42f98546f4038ec7e83727e3d5a80802f7f84f45
themanaff <ahmedovmenaf@gmail.com>
2024-08-28 10:01:42 +0400

    RegisterService implemented

--- userservice/src/main/java/com/fflay/userservice/service/impl/RegisterServiceImpl.java
+++ userservice/src/main/java/com/fflay/userservice/service/impl/RegisterServiceImpl.java
@@ -3,6 +3,9 @@ package com.fflay.userservice.service.impl;
 import com.fflay.userservice.exception.UserAlreadyExistException;
 import com.fflay.userservice.model.user.User;
 import com.fflay.userservice.model.user.dto.request.RegisterRequest;
+import com.fflay.userservice.model.user.entity.UserEntity;
+import com.fflay.userservice.model.user.mapper.RegisterRequestToUserEntityMapper;
+import com.fflay.userservice.model.user.mapper.UserEntityToUserMapper;
 import com.fflay.userservice.repository.UserRepository;
 import com.fflay.userservice.service.RegisterService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,8 @@ import org.springframework.stereotype.Service;
 public class RegisterServiceImpl implements RegisterService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RegisterRequestToUserEntityMapper registerRequestToUserEntityMapper = RegisterRequestToUserEntityMapper.initialize();
+    private final UserEntityToUserMapper userEntityToUserMapper = UserEntityToUserMapper.initialize();
 
     /**
      * Registers a new user based on the provided {@link RegisterRequest}.
@@ -33,6 +38,9 @@ public class RegisterServiceImpl implements RegisterService {
         if(userRepository.existsUserEntityByEmail(registerRequest.getEmail())){
             throw new UserAlreadyExistException("The email is already used for another admin: " + registerRequest.getEmail());
         }
-
+        final UserEntity userEntityToBeSave = registerRequestToUserEntityMapper.mapForSaving(registerRequest);
+        userEntityToBeSave.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        var savedUserEntity = userRepository.save(userEntityToBeSave);
+        return userEntityToUserMapper.map(savedUserEntity);
     }
 }
