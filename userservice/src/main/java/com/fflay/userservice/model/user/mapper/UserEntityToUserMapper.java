e95a00b46ae70069d69e6cd695e869e319029e92
themanaff <ahmedovmenaf@gmail.com>
2024-08-28 09:53:55 +0400

    Some mapper classes added for user registration

---
+++ userservice/src/main/java/com/fflay/userservice/model/user/mapper/UserEntityToUserMapper.java
@@ -0,0 +1,37 @@
+package com.fflay.userservice.model.user.mapper;
+
+import com.fflay.userservice.model.common.mapper.BaseMapper;
+import com.fflay.userservice.model.user.User;
+import com.fflay.userservice.model.user.entity.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Mapper interface for converting between {@link UserEntity} and {@link User}.
+ * This mapper handles the transformation of user entity data into a user domain model.
+ */
+@Mapper
+public interface UserEntityToUserMapper extends BaseMapper<UserEntity, User> {
+
+    /**
+     * Maps a {@link UserEntity} to a {@link User}.
+     * This method performs the mapping of the user entity to the corresponding user domain model.
+     *
+     * @param source the user entity to be mapped
+     * @return a {@link User} with mapped values
+     */
+    @Override
+    User map(UserEntity source);
+
+    /**
+     * Initializes the {@link UserEntityToUserMapper} mapper.
+     *
+     * @return a new instance of {@link UserEntityToUserMapper}
+     */
+    static UserEntityToUserMapper initialize(){
+        return Mappers.getMapper(UserEntityToUserMapper.class);
+    }
+}
