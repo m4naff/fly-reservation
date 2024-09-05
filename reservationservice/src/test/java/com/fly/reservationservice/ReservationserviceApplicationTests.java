89b5602dc0ad1675f07c4be1e9bddcace44dc97d
themanaff <ahmedovmenaf@gmail.com>
2024-09-05 12:44:23 +0400

    Rerservation service created

---
+++ reservationservice/src/test/java/com/fly/reservationservice/ReservationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fly.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
