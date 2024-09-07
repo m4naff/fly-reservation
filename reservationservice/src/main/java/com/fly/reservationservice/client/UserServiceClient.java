5c5f9f728b9d44deddbbbb3edc1c91f579e560cb
themanaff <ahmedovmenaf@gmail.com>
2024-09-07 18:56:32 +0400

    FeignClient is completed

--- reservationservice/src/main/java/com/fly/reservationservice/client/UserServiceClient.java
+++ reservationservice/src/main/java/com/fly/reservationservice/client/UserServiceClient.java
@@ -1,6 +1,11 @@
 package com.fly.reservationservice.client;
 
+import com.fly.reservationservice.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Feign client interface named {@link UserServiceClient} for interacting with the User Service.
@@ -8,6 +13,22 @@ import org.springframework.cloud.openfeign.FeignClient;
  */
 @FeignClient(name = "userservice", path = "api/v1/users",configuration = FeignClientConfig.class)
 public interface UserServiceClient {
+    /**
+     * Validates the provided token by making a request to the user service.
+     *
+     * @param token the token to validate
+     */
+    @PostMapping("/validate-token")
+    void validateToken(@RequestParam String token);
+
+    /**
+     * Retrieves authentication information based on the provided token.
+     *
+     * @param token the token to use for retrieving authentication information
+     * @return {@link UsernamePasswordAuthenticationToken} containing authentication details
+     */
+    @GetMapping("/authenticate")
+    UsernamePasswordAuthenticationToken getAuthentication(@RequestParam String token);
 
 
 }
